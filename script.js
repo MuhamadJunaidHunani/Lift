0ee31cef38dba66fcb5f262b01edd7be70df21a9
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2024-07-31 02:21:11 +0500

    Make Position Tracker

--- script.js
+++ script.js
@@ -21,14 +21,25 @@ const floors = [
     },
 ];
 
-let currentFloor = floors[floors.length - 1];
-const callingFloors = [];
-let isLiftMoving = false;
-
 const liftContainer = document.querySelector(".liftContainer");
 const leftDoor = document.querySelector(".leftDoor");
 const rightDoor = document.querySelector(".rightDoor");
 const dailBtn = document.querySelectorAll(".dailBtn");
+const floorContainer = document.querySelectorAll(".floors");
+
+let currentFloor = floors[floors.length - 1];
+const callingFloors = [];
+let isLiftMoving = false;
+let direction = false;
+
+floorContainer.forEach((locationPlate) => {
+    locationPlate.innerHTML = `
+        <div class="locationPlate">
+            <i class="fa-solid fa-caret-down upIcon"></i>
+            <p class="floorNo">${currentFloor.floor}</p>
+            <i class="fa-solid fa-caret-up downIcon"></i>
+        </div>`;
+});
 
 dailBtn.forEach((btn, index) => {
     btn.addEventListener("click", (e) => {
@@ -55,13 +66,41 @@ function setInLine() {
 }
 function moveLift(floor, callback) {
     const floorDifference = Math.abs(currentFloor.index - floor.index);
+    direction = currentFloor.index > floor.index ? "up" : "down";
+    floorContainer.forEach((locationPlate) => {
+        locationPlate.innerHTML = `
+            <div class="locationPlate">
+                <i class="fa-solid fa-caret-down upIcon ${direction==="down"?"active":''}"></i>
+                <p class="floorNo">${currentFloor.floor}</p>
+                <i class="fa-solid fa-caret-up downIcon ${direction==="up"?"active":''}"></i>
+            </div>`;
+    });
 
+    let tempIndex = currentFloor.index;
+    const intervalId = setInterval(() => {
+
+        if (floor.index < currentFloor.index) {
+            tempIndex--;
+            currentFloor = floors[tempIndex]
+        }
+        else if (floor.index > currentFloor.index) {
+            tempIndex++;
+            currentFloor = floors[tempIndex]
+        }
+        else {
+            clearInterval(intervalId);
+            return
+        }
+        document.querySelectorAll(".floorNo").forEach((no)=>{
+            no.innerHTML = currentFloor.floor
+        })
+        
+    }, 1000);
     liftContainer.style.transition = `top ${floorDifference}s linear`;
     liftContainer.style.top = floor.position;
 
     setTimeout(() => {
-        currentFloor = floor;
-        setTimeout(()=>{
+        setTimeout(() => {
             leftDoor.style.left = "-50%"
             rightDoor.style.left = "100%"
             setTimeout(() => {
@@ -69,7 +108,7 @@ function moveLift(floor, callback) {
                 rightDoor.style.left = "50%"
                 dailBtn[floor.index].classList.remove("selectedBtn");
             }, 2500)
-        },1000)
+        }, 1000)
         callback();
     }, floorDifference * 1000);
 }
